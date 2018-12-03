e877b676b25f18e088f0f37490aba89dea72f77c
muraleo <muraleo80923@gmail.com>
2018-12-03 09:25:36 -0500

    fix url bug

--- project/books/urls.py
+++ project/books/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/api/books', views.BookListCreate.as_view()),
+    path('api/books', views.BookListCreate.as_view()),
 ]
