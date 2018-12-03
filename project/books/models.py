60d9ca4922017eb203dfa427cca45f799f520c9a
muraleo <muraleo80923@gmail.com>
2018-12-03 08:40:54 -0500

    create books table

--- project/books/models.py
+++ project/books/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length = 100)
+    genre = models.CharField(max_length = 30)
+    rating = models.DecimalField(max_digits=1, decimal_places=0)
+    comment = models.CharField(max_length = 200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    end_at = models.DateTimeField()
